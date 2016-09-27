46404353eb4adb19877f1123df95a8846746714a
Tzeng Jiun-You <joe5343281@gmail.com>
2016-09-27 18:58:46 +0800

    Add files via upload

---
+++ root.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from sympy import limit
+from sympy import Symbol
+
+def EvalSlope(func,x):
+    dx = 0.000001
+    dy = func(x + dx) - func(x)
+    
+    return dy/dx
+
+def NewtonMethond(func, x):
+    slope = EvalSlope(func, x)
+    xn = x - (func(x)/slope)
+    
+    if((func(xn) > 0.5) or (func(xn) < -0.5)):
+        return NewtonMethond(func, xn)
+    else:
+        return xn
+    
+def f(x):
+    return ((x**14) - 1234567890123456)
+    
+def main():
+    print(NewtonMethond(f, 100))
+
+main() 
