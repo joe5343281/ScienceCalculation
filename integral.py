ea745aa6f27c56279af9cb337ac900ffe1bd9890
Osakamagi <joe5343281@gmail.com>
2016-09-28 18:15:06 +0800

    Add files via upload

---
+++ integral.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+def interal(f, a, b):
+    sum = 0.0
+    dx = 0.001
+    while(a < (b - 0.001)): 
+        sum += f(b - dx) * dx
+        b -= dx
+    
+    return sum
+    
+def f(x):
+    return (3 * (x**2))
+    
+def main():
+    print("f(x)=3*(x^3),from 0 to 10")
+    print(interal(f, 0, 10))
+    
+main()
